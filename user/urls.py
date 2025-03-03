53e4fd16cd60bdeb475c084d03ce5555dd3f59da
Oleksii Ivanov <oleks4335@gmail.com>
2025-03-03 11:13:36 +0100

    Add user authentication and registration endpoints
    - Configure URL routing for user-related API endpoints
    - Implement UserSerializer with create and update methods
    - Add views for user creation and profile management
    - Integrate JWT authentication for user endpoints

---
+++ user/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from .views import UserCreateView, ManageUserView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
+
+urlpatterns = [
+    path("create/", UserCreateView.as_view(), name="create"),
+    path("me/", ManageUserView.as_view(), name="me"),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+]
--- user/views.py
+++ user/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from user.serializers import UserSerializer
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+class UserCreateView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [permissions.AllowAny]
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
