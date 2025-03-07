e64bce1f21cc7bd67c6dd7fb58489f723d703a60
Oleksii Ivanov <oleks4335@gmail.com>
2025-03-07 20:58:13 +0100

    Configure Train model in Django admin

--- station/admin.py
+++ station/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Station, Route
+from .models import Station, Route, Train
 
 
 @admin.register(Station)
@@ -14,3 +14,10 @@ class RouteAdmin(admin.ModelAdmin):
     list_display = ("origin_station", "destination_station", "distance_km")
     search_fields = ("origin_station__name", "destination_station__name")
     list_filter = ("origin_station__city", "destination_station__city")
+
+
+@admin.register(Train)
+class TrainAdmin(admin.ModelAdmin):
+    list_display = ("name", "number", "train_type")
+    search_fields = ("name", "number")
+    list_filter = ("train_type",)
